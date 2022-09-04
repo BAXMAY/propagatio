e9191a89b830f9c4fb4700a0ea4d5272af9fe876
Deliciae <baramee1407@gmail.com>
2022-09-04 14:52:28 +0700

    Change sheet id Aug 2022

--- charta/export_spreadsheet.py
+++ charta/export_spreadsheet.py
@@ -39,7 +39,7 @@ REQUIRED_ARGUMENT_NUM = 0
 CREDENTIAL_FILE_PATH = 'credentials.json'
 CONFIG_FILE_PATH = 'config.toml'
 # NOTE: Email: mamamia-spread@stat-spreadsheet.iam.gserviceaccount.com
-SPREADSHEET_ID = '1FemovUJxuaCvxuxloWi7uzoKDdEQTkSRmMehZmUM9gI'
+SPREADSHEET_ID = '1PVGB91CD-82Lhv2bNah6NrujkxbgWiCutN0SK7gBl3E'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 log = logging.getLogger(__name__)
